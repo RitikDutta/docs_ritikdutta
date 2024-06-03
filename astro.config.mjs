543eeb1238c7fc762cc412336269542c9d844ff0
ritik dutta <ritikduttagd@gmail.com>
2024-06-04 00:07:27 +0530

    added config to deploy on git page

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://ritikdutta.github.io',
+	base: 'docs_ritikdutta',
 	integrations: [
 		starlight({
 			title: 'Designs',
