e31696d2ab22373d3112a4eb761c71f2579e60a7
ritik dutta <ritikduttagd@gmail.com>
2024-06-03 23:45:43 +0530

    added new pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,24 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'Designs',
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/ritikdutta.com',
 			},
 			sidebar: [
+				{
+					label: 'CWEM Docs',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'CWEM Guide', link: '/guides/company_work_environment_management/' },
+						{ label: 'HLD', link: '/guides/hld/' },
+						{ label: 'LLD', link: '/guides/lld/' },
+						{ label: 'Architecture', link: '/guides/architecture/' },
+						{ label: 'Wireframe', link: '/guides/wireframe/' },
+						{ label: 'KPI', link: '/guides/kpi/' },
+						
+					],
+				},
 				{
 					label: 'Guides',
 					items: [
